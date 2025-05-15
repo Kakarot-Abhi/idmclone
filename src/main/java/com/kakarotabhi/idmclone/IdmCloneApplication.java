46a959fab2fa0f30cd575f74079ca950ff15c782
Abhishek Mishra <mishraabhishek79333@gmail.com>
2025-05-16 01:12:34 +0530

    initial setup

---
+++ src/main/java/com/kakarotabhi/idmclone/IdmCloneApplication.java
@@ -0,0 +1,13 @@
+package com.kakarotabhi.idmclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakarotabhi/idmclone/IdmCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakarotabhi.idmclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
